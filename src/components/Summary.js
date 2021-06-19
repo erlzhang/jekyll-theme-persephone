dd4447d15a416776e97e4408eb5562faec89faa7
erlzhang <zhangshiyu1992@hotmail.com>
2021-06-19 17:29:19 +0800

    chapter页面做无刷新切换章节

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -10,29 +10,31 @@ export default class extends Component {
     this.main = document.getElementById("bookMain")
     this.isOpen = false
     this.current = document.querySelector(".chapter-link.active");
-    const summary = document.getElementsByClassName("summary")[0];
-    summary.addEventListener("click", (e) => {  
-      const target = e.target;
-      this.current.classList.remove("active");
-      target.classList.add("active");
 
-      this.current = target;
-    })
-    this.main.addEventListener("click", (e) => {
-      console.log("mian on click", e, e.target, e.target.classList)
-      const target = e.target;
-      if (target.classList.contains("navigation")) {
-        const href = target.href;
-        console.log("href", href);
-        const item = document.querySelector(`.chapter[href=${href}]`);
-        console.log("item", item);
-        if (item) {
-          item.classList.add("active");
-          this.current.classList.remove("active");
-          this.current = item;
+    const content = document.getElementById("chapter-content");
+
+    const callback = (mutationsList, observer) => {
+      for(let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          if (mutation.addedNodes.length > 0) {
+            const child = Array.from(mutation.addedNodes).find(node => node.tagName === "ARTICLE");
+            if (child) {
+              const name = child.getAttribute("data-chapter");
+              const item = document.getElementById(`chapter-${name}`);
+              if (item) {
+                item.classList.add("active");
+                this.current.classList.remove("active");
+                this.current = item;
+              }
+            }
+          }
         }
       }
-    })
+    }
+
+    const observer = new MutationObserver(callback)
+
+    observer.observe(content, { attributes: true, childList: true, subtree: true })
 
     if( this.toggler ) {
       this.toggler.addEventListener("click", (event) => {
